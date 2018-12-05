7182f427d513c60d4977acdc76298237b1ec11d6
Nicholas Tzavaras <nicholas_tzavaras@yahoo.com>
2018-12-04 21:26:00 -0700

    Fixed the extra call to ngOnInit() in book-detail

--- src/app/book-detail/book-detail.component.spec.ts
+++ src/app/book-detail/book-detail.component.spec.ts
@@ -2,15 +2,16 @@ import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { BookDetailComponent } from './book-detail.component';
 
+
 describe('BookDetailComponent', () => {
   let component: BookDetailComponent;
   let fixture: ComponentFixture<BookDetailComponent>;
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [ BookDetailComponent ]
+      declarations: [BookDetailComponent]
     })
-    .compileComponents();
+      .compileComponents();
   }));
 
   beforeEach(() => {
